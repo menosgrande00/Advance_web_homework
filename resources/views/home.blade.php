5aaa69ad98b7ba951fa1b28332e4bd672540eedb
Ömer Faruk Önal <omerfarukonal@Omer-MacBook-Air.local>
2026-05-17 22:31:27 +0300

    feat: project start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Project</title>
+</head>
+<body>
+    <h1>Laravel Project Start</h1>
+
+    <p>This project start for laravel homework.</p>
+</body>
+</html>
